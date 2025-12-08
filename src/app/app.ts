c10f2519467e673a6b9725b5f389a38e6552281b
Tisa <tisatandel@gmail.com>
2025-12-08 22:01:41 +0530

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+  template:`<h1>Hello</h1>`,
+  styles:'h1{color:red;}'
 })
 export class App {
   protected readonly title = signal('myfirstapp');
