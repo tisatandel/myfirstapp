995de16b7d540e2d15bca0e90a4147d4fe80c071
Tisa <tisatandel@gmail.com>
2025-12-09 16:43:26 +0530

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,header],
+  imports: [RouterOutlet,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
