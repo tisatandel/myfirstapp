1f1cdb114549bb4fa884a7229239c07e53efa8c9
Tisa <tisatandel@gmail.com>
2025-12-09 17:20:49 +0530

    app:

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { Main } from './main/main';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
+  imports: [RouterOutlet,Header,Footer,Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
