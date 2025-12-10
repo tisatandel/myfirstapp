a1ceecbaac8a1840e4e63d889ee70f50ffc912ad
Tisa <tisatandel@gmail.com>
2025-12-11 00:54:48 +0530

    app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { Main } from './main/main';
 import { Fruits } from './fruits/fruits';
 import { Contact } from './contact/contact';
 import { Error } from './error/error';
+import { Privacy } from './privacy/privacy';
+import { Terms } from './terms/terms';
+import { Support } from './support/support';
 export const routes: Routes = [
     {
     path:'main',component:Main
@@ -17,7 +20,15 @@ export const routes: Routes = [
     {
     path:'contact',component:Contact
     },
-     
+    {
+    path:'privacy',component:Privacy
+    },
+    {
+    path:'terms',component:Terms
+    },
+    {
+    path:'support',component:Support
+    },
     {
         path:'',
         redirectTo:'main',
