5ea702a9edec480ba47e06c157ecd42cf70a197a
Tisa <tisatandel@gmail.com>
2025-12-10 23:42:42 +0530

    app

--- src/app/error/error.ts
+++ src/app/error/error.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-error',
