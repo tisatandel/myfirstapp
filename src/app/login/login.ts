d4fbad82eea66814e4bab7a7e363ef915ef08b61
Tisa <tisatandel@gmail.com>
2025-12-11 15:29:13 +0530

    app

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ RouterOutlet,RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
